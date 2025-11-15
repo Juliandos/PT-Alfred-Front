63be0eb44644e400eed94dc091cffcf11c079ff6
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-11-15 09:56:09 -0500

    Store para almacenar el estado de la aplicación.

---
+++ app/stores/airport.store.ts
@@ -0,0 +1,43 @@
+import { create } from "zustand";
+import { listAirports, getAirportById } from "../services/aviationstack.service";
+
+interface AirportStore {
+  airports: any[];
+  loading: boolean;
+  selectedAirport: any | null;
+
+  fetchAirports: (params: { search?: string; page: number }) => Promise<void>;
+  fetchAirportDetails: (id: string) => Promise<void>;
+}
+
+export const useAirportStore = create<AirportStore>((set) => ({
+  airports: [],
+  loading: false,
+  selectedAirport: null,
+
+  fetchAirports: async ({ search, page }) => {
+    set({ loading: true });
+
+    try {
+      const data = await listAirports({
+        search,
+        limit: 10,
+        offset: (page - 1) * 10,
+      });
+
+      set({ airports: data.data });
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  fetchAirportDetails: async (id) => {
+    set({ loading: true });
+    try {
+      const data = await getAirportById(id);
+      set({ selectedAirport: data.data?.[0] ?? null });
+    } finally {
+      set({ loading: false });
+    }
+  },
+}));
