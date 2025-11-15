3b83bd9ccddfbf9cfba861fcdcc58b8d7f18b01b
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-11-15 09:59:50 -0500

    Tipo de interfaz para representar un aeropuerto.

---
+++ app/services/types.ts
@@ -0,0 +1,10 @@
+export interface Airport {
+  airport_name: string;
+  iata_code: string;
+  icao_code: string;
+  latitude: number;
+  longitude: number;
+  country_name: string;
+  city_iata: string;
+  timezone: string;
+}
