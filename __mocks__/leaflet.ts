2c8995467e684ea2adc4b5af07cb16b6eff8a252
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-11-16 16:42:01 -0500

    Actualizaciones de Jest para adaptarse a React 19

---
+++ __mocks__/leaflet.ts
@@ -0,0 +1,11 @@
+export const Icon = jest.fn(() => ({}));
+export const Map = jest.fn(() => ({}));
+export const TileLayer = jest.fn(() => ({}));
+export const Marker = jest.fn(() => ({}));
+
+export default {
+  Icon,
+  Map,
+  TileLayer,
+  Marker,
+};
--- jest.config.ts
+++ jest.config.ts
@@ -1,38 +1,55 @@
 import type { Config } from 'jest';
-import nextJest from 'next/jest';
 
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-});
-
-// Add any custom config to be passed to Jest
+// Configuración de Jest sin next/jest para evitar problemas de módulos
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^@/app/(.*)$': '<rootDir>/app/$1',
+  
+  // Transformaciones
+  transform: {
+    '^.+\\.(t|j)sx?: ['@swc/jest', {
+      jsc: {
+        transform: {
+          react: {
+            runtime: 'automatic',
+          },
+        },
+      },
+    }],
   },
+  
+  moduleNameMapper: {
+    '^@/(.*): '<rootDir>/$1',
+    '^@/app/(.*): '<rootDir>/app/$1',
+    '\\.(css|less|scss|sass): 'identity-obj-proxy',
+    '^.+\\.module\\.(css|sass|scss): 'identity-obj-proxy',
+    '^leaflet: '<rootDir>/__mocks__/leaflet.ts',
+    '^react-leaflet: '<rootDir>/__mocks__/react-leaflet.tsx',
+  },
+  
   testMatch: [
     '**/__tests__/**/*.test.[jt]s?(x)',
     '**/?(*.)+(spec|test).[jt]s?(x)',
   ],
+  
   collectCoverageFrom: [
     'app/**/*.{js,jsx,ts,tsx}',
     '!app/**/*.d.ts',
     '!app/**/*.stories.{js,jsx,ts,tsx}',
     '!app/**/_*.{js,jsx,ts,tsx}',
   ],
+  
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
     '<rootDir>/.next/',
   ],
+  
   transformIgnorePatterns: [
-    '/node_modules/(?!(react-leaflet|@react-leaflet)/)',
+    'node_modules/(?!(react-leaflet|@react-leaflet)/)',
   ],
+  
+  moduleDirectories: ['node_modules', '<rootDir>/'],
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config);
+export default config;
